6484373ded15e42027d668a0722fbf36d92b8ea1
= <=>
2025-10-11 22:31:17 +0530

    updated to load model from MLflow directly

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import pymongo as pym
 import requests
 import os 
 import numpy as np
+from dotenv import load_dotenv 
+import mlflow 
+
+load_dotenv()
 
 uri=os.environ.get('MONGO_URI')
+
 app=Flask(__name__)
+
 client=pym.MongoClient(uri)
 db=client.Resume_Screening
 Model_results=db.Model_Results
@@ -31,34 +37,48 @@ def upload():
 
 @app.route('/processing')
 def processing():
+    Result_not_found_in_db=False
     experience=int(buffer.get('Experience'))
     projects=int(buffer.get('Projects'))
+    type=buffer.get('Type of Processing')
+
+
     input_to_model=[[experience,projects]]
+    data_from_db=Model_results.find_one({'Years of Experience':experience,'Number of Projects':projects},['Result'])
 
-    if buffer.get('Type of Processing')=='AI Score':
-        # result=requests.post('http://regression:5000/make_prediction',json={'exp':experience,'projs':projects})
-        # prediction=result.json()['prediction']
-        regressor=pickle.load(open(os.path.join('Models','R_model.pkl'),'rb'))
-        ai_score=regressor.predict(input_to_model)[0]
-        prediction=np.exp(-ai_score)
-        prediction=round(float(prediction),0)
-        if prediction>100:
-            prediction=100
-        elif prediction<0:
-            prediction=0
-        result=f'The AI Score for you is {prediction}'
-    else:
-        # result=requests.post('http://classification:5000/make_prediction',json={'exp':experience,'projs':projects})
-        # prediction=result.json()['prediction']
-        classifier=pickle.load(open(os.path.join('Models','C_model.pkl'),'rb'))
-        prediction=classifier.predict(input_to_model)
-        if prediction[0]=='Hire':
-            result=f'We predict that you will be selected'
+    if data_from_db is not None:
+        if ('AI' in data_from_db['Result'].split(' ')) and (type=='AI Score'):
+            result=data_from_db['Result']
+        elif ('predict' in data_from_db['Result'].split(' ')) and (type=="Recruiter Decision"):
+            result=data_from_db['Result']
         else:
-            result=f'We predict that you will be rejected'
+            Result_not_found_in_db=True
+        
+    if Result_not_found_in_db:  
+        if type=='AI Score':
+            # result=requests.post('http://regression:5000/make_prediction',json={'exp':experience,'projs':projects})
+            # prediction=result.json()['prediction']
+            regressor=mlflow.sklearn.load_model('models:/Regression-Resume-Screening-LR/Latest')
+            ai_score=regressor.predict(input_to_model)[0]
+            prediction=np.exp(-ai_score)
+            prediction=round(float(prediction),0)
+            if prediction>100:
+                prediction=100
+            elif prediction<0:
+                prediction=0
+            result=f'The AI Score for you is {prediction}'
+        else:
+            # result=requests.post('http://classification:5000/make_prediction',json={'exp':experience,'projs':projects})
+            # prediction=result.json()['prediction']
+            classifier=mlflow.sklearn.load_model('models:/Classification-Resume-Screening-Stacking/Latest')
+            prediction=classifier.predict(input_to_model)
+            if prediction[0]=='Hire':
+                result=f'We predict that you will be selected'
+            else:
+                result=f'We predict that you will be rejected'
 
 
-    Model_results.insert_one({'Years of Experience':experience,'Number of Projects':projects,'Result':result})
+        Model_results.insert_one({'Years of Experience':experience,'Number of Projects':projects,'Result':result})
     return render_template('result.html',prediction=result)   
 
 
